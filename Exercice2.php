87667d7645266c0c96808497fdd3869e531e71bf
TOM SIKORA <siko0001@2A4F1-31UPC108.ad-urca.univ-reims.fr>
2023-01-24 15:02:44 +0100

    Exercice 2

---
+++ Exercice2.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+# Question 7
+function afficheNomPrenom(string $nom, string $prenom): void
+{
+    echo "$nom $prenom \n";
+}
+afficheNomPrenom('Sikora', 'Tom');
+
+# Question 8
+function calculSomme(int $a, int $b, int $c): int
+{
+    $resultat = $a + $b + $c ;
+    return $resultat ;
+}
+echo calculSomme(2, 3, 4)."\n";
+
+# Question 9
+function getAge(): int
+{
+    return intval(readline("Saisissez votre âge !"));
+}
+# echo getAge();
+
+# Question 10
+function plusGrand(int $min, int $max): bool
+{
+    $hasard1 = rand ($min, $max);
+    $hasard2 = rand ($min, $max);
+    echo "$hasard1 / $hasard2\n";
+    $res = false;
+    if ($hasard1 > $hasard2) {
+        $res = true;
+    }
+    return $res;
+}
+function decodeBooleen(bool $bool): string
+{
+    $res = "True";
+    if (!$bool) {
+        $res = "False";
+    }
+    return $res;
+}
+echo decodeBooleen(plusGrand(1, 10));
